695084b22ce62481327110e50212f1cb0c6f5e69
wgroc <william.groc@sogeti.com>
2026-01-29 16:45:58 +0100

    Complete 3.1

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,30 @@
-use std::fs;
+use std::{fs, str};
 
-fn search_joltage() -> i32 {
-    0
+fn search_joltage(line: &str) -> u32 {
+    let mut max_first_number = 0;
+    let mut max_second_number = 0;
+
+    let string_length = line.len();
+
+    for (i, c) in line.char_indices() {
+        if let Some(current_number) = c.to_digit(10) {
+            if string_length - 1 > i && current_number > max_first_number {
+                max_first_number = current_number;
+                max_second_number = 0;
+            } else if (max_first_number * 10 + current_number)
+                > (max_first_number * 10 + max_second_number)
+            {
+                max_second_number = current_number;
+            }
+        }
+    }
+
+    let result = max_first_number * 10 + max_second_number;
+    println!(
+        "Result for line {}: first: {}, last: {}, result: {}",
+        line, max_first_number, max_second_number, result
+    );
+    result
 }
 
 pub fn run() -> Result<(), std::num::ParseIntError> {
@@ -9,12 +32,14 @@ pub fn run() -> Result<(), std::num::ParseIntError> {
 
     let mut counter = 0;
 
-    for lin in contents.lines() {}
+    for lin in contents.lines() {
+        counter += search_joltage(lin)
+    }
 
     println!("Counter: {}", counter);
     Ok(())
 }
 
 fn read_file() -> String {
-    fs::read_to_string("./data_day2.txt").expect("Failed to read file")
+    fs::read_to_string("./data_day3.txt").expect("Failed to read file")
 }
